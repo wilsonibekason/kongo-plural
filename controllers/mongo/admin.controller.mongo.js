a085abfea05d73a27ccff516d8d7e606babda0f9
wilsonibekason <wanbekgithub@gmail.com>
2023-04-03 22:25:57 +0100

    add CRUD operations for product model

--- controllers/mongo/admin.controller.mongo.js
+++ controllers/mongo/admin.controller.mongo.js
@@ -1,10 +1,46 @@
 const ProductMongo = require("../../models/mongoose/product.mongo");
+const mongodb = require("mongodb");
 
 const getAdminController = (req, res, next) => {
   return ProductMongo.fetchAll().then((__) => {});
 };
-const editAdminController = (req, res, next) => {};
-const postEditAdminController = (req, res, next) => {};
+const editAdminController = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) return res.redirect("/");
+  const prodId = req.params.productId;
+  ProductMongo.findById(prodId)
+    .then(() => {
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Products",
+        path: " /admin/edit-product",
+        editing: editMode,
+        product,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+const postEditAdminController = (req, res, next) => {
+  const {
+    productId: prodId,
+    title: updatedTitle,
+    imageUrl: updatedImageUrl,
+    price: updatedPrice,
+    description: updatedDesc,
+  } = req.body;
+
+  const products = new ProductMongo(
+    updatedTitle,
+    updatedDesc,
+    updatedImageUrl,
+    updatedPrice,
+    // new mongodb.ObjectId(prodId)
+    prodId
+  );
+  products
+    .save()
+    .then((_) => res.redirect("/admin/products"))
+    .catch((err) => console.log(err));
+};
 const addAdminController = (req, res, next) => {
   const { title, imageUrl, description, price } = req.body;
   const product = new ProductMongo(title, description, imageUrl, price);
@@ -14,7 +50,12 @@ const addAdminController = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 const getAdminProductsController = (req, res, next) => {};
-const deleteProductController = (req, res, next) => {};
+const deleteProductController = (req, res, next) => {
+  const { productId } = req.body;
+  return ProductMongo.deleteById(productId)
+    .then((_) => res.redirect("/admin/products"))
+    .catch((err) => console.log(err));
+};
 
 module.exports = {
   getAdminMongo: getAdminController,
--- models/mongoose/product.mongo.js
+++ models/mongoose/product.mongo.js
@@ -1,19 +1,28 @@
 const { getDB } = require("../../util/mongodb");
+const mongo = require("mongodb");
 const generateCollectionHook = require("../../hooks/generateCollectionMethod");
 
 class ProductMongo {
-  constructor(title, description, imageUrl, price) {
+  constructor(title, description, imageUrl, price, id, newId) {
     this.title = title;
     this.description = description;
     this.imageUrl = imageUrl;
     this.price = price;
+    this._id = id ? new mongo.ObjectId(id) : null;
+    this._newId = newId;
   }
 
   save() {
     const db = getDB();
-    return db
-      .collection("products")
-      .insertMany(this)
+    let dbOp;
+    if (!this._id) {
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: new mongo.ObjectId(this._id) }, { $set: this });
+    } else {
+      dbOp = db.collection("products").insertMany(this);
+    }
+    return dbOp
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
   }
@@ -34,6 +43,16 @@ class ProductMongo {
       .next()
       .then((___) => ___)
       .catch(err >= console.log(err));
+  }
+  static deleteById(prodId) {
+    let db = getDB();
+    return generateCollectionHook(db, "products")
+      .deleteOne({ _id: new mongo.ObjectId(prodId) })
+      .then((_) => {
+        let prodTitle = this.findById(prodId).then(({ title }) => title);
+        console.log(`${prodTitle} has been deleted `);
+      })
+      .catch((err) => console.log(err));
   }
 }
 
